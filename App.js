f52e9096ddd88030bfd61f2e2f932354dad8efb9
susan2099 <59705471+susan2099@users.noreply.github.com>
2024-02-04 16:44:13 -0800

    implemented react native maps

--- App.js
+++ App.js
@@ -1,20 +1,105 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+       style={styles.map}
+       showsUserLocation={true}
+       region={{
+         latitude: 37.78525,
+         longitude: -122.4124,
+         latitudeDelta: 0.05,
+         longitudeDelta: 0.0121,
+       }}
+     >
+      <Marker
+        coordinate={{ latitude : 37.78525 , longitude : -122.4124 }}
+        title={'Hello'}
+        description={'World'}
+        image={require('./marker.png')}
+      >
+      </Marker>
+     </MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    ...StyleSheet.absoluteFillObject,
+    height: 900,
+    width: 400,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center',
+
   },
-});
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+ });
+
+
+ //step 1:
+{/* <MapView
+  style={styles.map}
+  region={{
+    latitude: 37.78525,
+    longitude: -122.4124,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.0121,
+}}
+>
+</MapView> */}
+
+//step 2:
+//create marker
+{/* <Marker
+coordinate={{ latitude : 37.78525 , longitude : -122.4124 }}
+title={'Hello'}
+description={'World'}
+>
+</Marker> */}
+
+//step 3:
+//marker draggable
+{/* <Marker draggable
+coordinate={{ latitude : 37.78525 , longitude : -122.4124 }}
+title={'Hello'}
+description={'World'}
+>
+</Marker> */}
+
+//step 4:
+//marker image update
+{/* <Marker draggable
+coordinate={{ latitude : 37.78525 , longitude : -122.4124 }}
+title={'Hello'}
+description={'World'}
+image={require('./marker.png')}
+>
+</Marker> */}
+
+//step 5:
+//show user location
+{/* <MapView
+  style={styles.map}
+  showsUserLocation={true}
+  region={{
+    latitude: 37.78525,
+    longitude: -122.4124,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.0121,
+  }}
+>
+  <Marker
+    coordinate={{ latitude : 37.78525 , longitude : -122.4124 }}
+    title={'Hello'}
+    description={'World'}
+    image={require('./marker.png')}
+  >
+  </Marker>
+</MapView> */}
