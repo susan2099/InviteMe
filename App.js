5594c0fa5e0eb94ab7ee66da13d70586b2b73e59
susan2099 <59705471+susan2099@users.noreply.github.com>
2024-02-04 21:37:46 -0800

    current location retrieved

--- App.js
+++ App.js
@@ -1,31 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+//import GetLocation, {Location, LocationErrorCode, isLocationError} from 'react-native-get-location';
+//import RNLocation from 'react-native-location';
+//import Geolocation from '@react-native-community/geolocation';
+
 
 export default function App() {
+
+  const [userLocation, setUserLocation] = useState({latitude: null, longitude:null});
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setUserLocation({latitude: location.coords.latitude, longitude: location.coords.longitude})
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
       <MapView
        style={styles.map}
        showsUserLocation={true}
        region={{
-         latitude: 37.78525,
-         longitude: -122.4124,
+         latitude: userLocation.latitude,
+         longitude: userLocation.longitude,
          latitudeDelta: 0.05,
          longitudeDelta: 0.0121,
        }}
      >
-      <Marker
-        coordinate={{ latitude : 37.78525 , longitude : -122.4124 }}
+       <Marker
+        coordinate={{ latitude : userLocation.latitude , longitude : userLocation.longitude}}
         title={'Hello'}
         description={'World'}
         image={require('./marker.png')}
       >
       </Marker>
+      {console.log(userLocation)}
      </MapView>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -34,8 +60,6 @@ const styles = StyleSheet.create({
     width: 400,
     justifyContent: 'flex-end',
     alignItems: 'center',
-    alignItems: 'center',
-
   },
   map: {
     ...StyleSheet.absoluteFillObject,
@@ -62,6 +86,7 @@ coordinate={{ latitude : 37.78525 , longitude : -122.4124 }}
 title={'Hello'}
 description={'World'}
 >
+
 </Marker> */}
 
 //step 3:
